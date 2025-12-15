505a321640c3c3b46b0b9255982a0a93103a028b
Gesgg <essus_94@live.cl>
2025-12-15 14:31:56 -0300

    Add files via upload

---
+++ API_IEC_170_N2/negocio/__init__.py
@@ -0,0 +1,13 @@
+from .negocio_user import (
+    obtener_users_api,
+    crear_user_api,
+    modificar_user_api,
+    eliminar_user_api,
+)
+
+from .negocio_post import (
+    obtener_posts_api,
+    crear_post_api,
+    modificar_post_api,
+    eliminar_post_api,
+)
